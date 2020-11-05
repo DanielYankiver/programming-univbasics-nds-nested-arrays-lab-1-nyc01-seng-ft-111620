7e1232f1a85945b115cf6eb6b7e4bb03b93144c3
Daniel Yankiver <dyankiver@gmail.com>
2020-11-05 16:46:51 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -44,17 +44,12 @@ end
 # Given any matrix (array of arrays), a row index and a column index, 
 # Return the matrix's content at that row and and column
 
-matrix = [
-     ["Grapefruit", "Pineapple", "Oranges", "Watermelon","Eggplant"].sort,
-     ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"].sort
-       ]
-
 def matrix_lookup(matrix, row, column)
-  row = matrix[0] && matrix[1]
-  column = matrix[0][0] && matrix[1][0] || matrix[0][1] && matrix[1][1] || matrix[0][2] || matrix[1][2]
+ produce_storage_room = [
+      ["Grapefruit", "Pineapple", "Oranges", "Watermelon","Eggplant"].sort,
+      ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"].sort
+        ]
 end
-
-p matrix_lookup(matrix, 1, 1)
 
 def matrix_update(matrix, row, column, new_value)
   # Given any matrix (array of arrays), a row index and a column index, 
